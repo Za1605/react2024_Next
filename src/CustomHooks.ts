1a0b68078366f65c79f75ebabef536478ac72ca5
YuliiaT <115080417+Za1605@users.noreply.github.com>
2024-05-29 23:38:03 +0300

    third homework

---
+++ src/CustomHooks.ts
@@ -0,0 +1,13 @@
+import React, {useState} from 'react';
+
+const UseToggle = (defaultValue: boolean) => {
+    const [value, setValue] = useState<boolean>(defaultValue);
+;
+    return (
+        <div>
+
+            </div>
+    );
+};
+
+export default CustomHooks;
