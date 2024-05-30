c61602d135f750bc6c133143e34a990adb1e949f
YuliiaT <115080417+Za1605@users.noreply.github.com>
2024-05-30 23:13:16 +0300

    third homework

--- src/CustomHooks.ts
+++ src/CustomHooks.ts
@@ -1,13 +1,35 @@
-import React, {useState} from 'react';
-
-const UseToggle = (defaultValue: boolean) => {
+import React, {useRef, useState} from 'react';
+//1. useToggle - custom React hook that allows a component to toggle a value between true and false
+//import { useState } from "react"
+const useToggle = (defaultValue: boolean) => {
     const [value, setValue] = useState<boolean>(defaultValue);
-;
-    return (
-        <div>
 
-            </div>
-    );
+   function funkTogle(valueSec: boolean) {
+       setValue(prevValue => !valueSec);
+   }
+return[value, funkTogle];
 };
 
-export default CustomHooks;
+let [value, funkTogle ] =useToggle(true);
+
+funkTogle(false);
+
+
+//2. usePrevious - hook that allows a component to keep track of the previous value of a variable
+
+const usePrevious =(value:any)=>{
+    const currentRef = useRef(value);
+    const prevRef = useRef();
+
+    if(currentRef.current !== value){
+        prevRef.current = currentRef.current;
+    }
+    return prevRef;
+}
+
+usePrevious(100);
+
+
+//3. useStorage - hook that allows a component to store a value in the browser's LocalStorage
+
+
