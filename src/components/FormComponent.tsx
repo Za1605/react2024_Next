b6581f9ad5e895450fe979b84ccf191a83f4b1a4
YuliiaT <115080417+Za1605@users.noreply.github.com>
2024-06-11 23:28:56 +0300

    hw5

--- src/components/FormComponent.tsx
+++ src/components/FormComponent.tsx
@@ -12,13 +12,26 @@ const FormComponent:FC = () =>{
  let {register,handleSubmit}= formObj;
 
 
-
+const save = ({body, title, userId}:IFormProps)=>{
+    console.log(formValues);
+    fetch('https://jsonplaceholder.typicode.com/posts',{
+        method: 'POST',
+        headers:{
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+        },
+        body: JSON.stringify({
+            title: formValues.username,
+            body: formValues.age,
+            userId:
+        })
+    })
+};
 
     return (
         <div>
-            <form onSubmit={handleSubmit((fff)=>{
-                console.log(fff);
-            })}>
+            <form onSubmit={handleSubmit(save)}>
+
                 <input type= "text" {...register('username')}/>
                 <input type= "number" {...register("age")}/>
                 <input type="text"{...register('password')}/>
