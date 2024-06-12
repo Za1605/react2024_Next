d4f6c8313763a1440dfc46a4c7cd13f1ee173e3a
YuliiaT <115080417+Za1605@users.noreply.github.com>
2024-06-12 23:37:12 +0300

    hw6

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -7,3 +7,9 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+//Створити сторінки та роути на них
+// users
+// posts
+// comments
+// При переході на кожну з них отримувати відповідні
+// відповіді від джейсонплейсхолдеру та виводити всі об'єкти
